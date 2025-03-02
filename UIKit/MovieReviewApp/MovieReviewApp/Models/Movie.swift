3595a64fc3def40f2b987ab65b9c3ef02add6135
Oneee_48 <chosuwon48@gmail.com>
2025-03-03 01:44:50 +0900

    리뷰 앱 수정

--- UIKit/MovieReviewApp/MovieReviewApp/Models/Movie.swift
+++
@@ -1,7 +0,0 @@
-//
-//  Movie.swift
-//  MovieReviewApp
-//
-//  Created by 조수원 on 3/2/25.
-//
-
--- UIKit/MovieReviewApp/MovieReviewApp/ViewModels/MovieViewModel.swift
+++
@@ -1,7 +0,0 @@
-//
-//  MovieViewModel.swift
-//  MovieReviewApp
-//
-//  Created by 조수원 on 3/2/25.
-//
-
