150071560f51611132af4e1d420fdf95c0ade9a2
Oneee_48 <chosuwon48@gmail.com>
2025-03-02 22:34:48 +0900

    영화 리뷰 앱 초기

---
+++ UIKit/MovieReviewApp/MovieReviewApp/Models/Review.swift
@@ -0,0 +1,7 @@
+//
+//  Review.swift
+//  MovieReviewApp
+//
+//  Created by 조수원 on 3/2/25.
+//
+
---
+++ UIKit/MovieReviewApp/MovieReviewApp/ViewModels/MovieViewModel.swift
@@ -0,0 +1,7 @@
+//
+//  MovieViewModel.swift
+//  MovieReviewApp
+//
+//  Created by 조수원 on 3/2/25.
+//
+
