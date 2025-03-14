6bf0958efa198441340e376f02b7d51bec2f8314
Oneee_48 <chosuwon48@gmail.com>
2025-03-14 17:03:46 +0900

    update

---
+++ test/test/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  test
+//
+//  Created by 조수원 on 3/10/25.
+//
+
+import UIKit
+import SwiftUI
+import SnapKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemIndigo
+    }
+}
+
+#Preview {
+    ViewController()
+}
