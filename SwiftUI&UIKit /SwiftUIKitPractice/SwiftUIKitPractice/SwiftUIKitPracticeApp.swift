46e35c842ce02ab6fef82c11ac7fb36be6a669c4
Oneee_48 <chosuwon48@gmail.com>
2025-03-27 10:23:30 +0900

    0327 수업

---
+++ SwiftUI&UIKit /SwiftUIKitPractice/SwiftUIKitPractice/SwiftUIKitPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKitPracticeApp.swift
+//  SwiftUIKitPractice
+//
+//  Created by 조수원 on 3/27/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKitPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
